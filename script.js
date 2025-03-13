e1e008e19900e9facff5d8fcbf0c3d263ca534b1
jain-d <jain.divyansh.0017@gmail.com>
2025-03-13 13:40:40 +0530

    Rendition 2

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 let addItems= document.body.querySelector(".add-items");
 let list = document.body.querySelector(".list");
+let clearButton = document.body.querySelector(".clear");
 
-addItems.addEventListener("click", () => {
+let timestamp;
+
+let agentMarkup = document.body.querySelector(".agent");
+let agent = navigator.userAgent;
+if (agent.includes("Safari")) {
+   agentMarkup.innerText = `This could be safari`;
+   if (!agent.includes("Chrome")) {
+      agentMarkup.innerText = `This is Safari`;
+   }
+}
+
+const handlerFunction = event => {
+   if (timestamp === undefined) {
+      addElements();
+      timestamp = new Date().getSeconds();
+   }
+   else if (new Date().getSeconds() >= (timestamp + 1) || new Date().getSeconds() < timestamp) {
+      addElements();
+      timestamp = new Date().getSeconds();
+   }
+   else {
+      console.log(`ClICK DEBOUNCED: for timestamp: ${timestamp}`);
+      event.preventDefault();
+      //timestamp = new Date().getSeconds();
+   }
+
+}
+
+addItems.addEventListener("click", handlerFunction);
+function addElements() {
    let item = document.createElement("li");
    item.innerText = "Adding one option";
    list.appendChild(item);
-});
+}
+let clk = new Event("click");
+let automatedClick = setInterval(() => {
+   addItems.dispatchEvent(clk);
+   console.log(`\n button clicked`);
+}, 500);
+
+clearButton.addEventListener("click", () => {
+   clearInterval(automatedClick);
+   addItems.removeEventListener("click", handlerFunction);
+   });
